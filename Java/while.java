6e6a884911a7fbdf69cc4963e95fc45317d9b80f
Criken02 <yagosanchezrochetti@gmail.com>
2023-03-15 12:18:53 -0300

    Tp 2

--- Java/while.java
+++ Java/while.java
@@ -1,10 +1,12 @@
-public class while {
+class While {
+
     public static void main(String[] args) {
-        int i = 1
-        while i < 100
-        System.out.println(i)
-        i++
+        int i=1;
+        System.out.println("Numeros del 1 al 100");
+        while(i<=100) {								
+            System.out.println(i);
+            i++;}								
     }
 
-    
-}
+}   
+
